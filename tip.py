4af53a84259a20203d929c4fabe5b19e88e35e01
Prithvi Kannivadi <43829107+prithvikannivadi@users.noreply.github.com>
2024-03-19 23:19:25 -0700

    Create tip.py

---
+++ tip.py
@@ -0,0 +1,21 @@
+# Use this program to calculate the tip for your meal
+
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    d = float(d.replace("$", ""))
+    return d
+
+
+def percent_to_float(p):
+    p = float(p.replace("%", ""))
+    p = p/100
+    return p
+
+if __name__ == "__main__":
+    main()
